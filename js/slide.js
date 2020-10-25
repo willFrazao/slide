816100c3536e74e7f35f73516cf75851a54b1d25
willFrazao <william.frazao91@gmail.com>
2020-10-25 12:23:16 -0300

    navegacao com os botoes prev e next

--- js/slide.js
+++ js/slide.js
@@ -1,6 +1,6 @@
 import debounce from './debounce.js'
 
-export default class Slide {
+export class Slide {
     constructor(slide, wrapper) {
         this.slide = document.querySelector(slide);
         this.wrapper = document.querySelector(wrapper);
@@ -118,7 +118,7 @@ export default class Slide {
         setTimeout(() => {
             this.slidesConfig();
             this.changeSlide(this.index.active);
-        },1000)
+        },1000);
     }
 
     addResizeEvent() {
@@ -129,6 +129,10 @@ export default class Slide {
         this.onStart = this.onStart.bind(this);
         this.onMove = this.onMove.bind(this);
         this.onEnd = this.onEnd.bind(this);
+
+        this.activePrevSlide = this.activePrevSlide.bind(this);
+        this.activeNextSlide = this.activeNextSlide.bind(this);
+
         this.onResize = debounce(this.onResize.bind(this), 200);
     }
 
@@ -138,6 +142,20 @@ export default class Slide {
         this.addSlideEvents();
         this.slidesConfig();
         this.addResizeEvent();
+        this.changeSlide(0);
         return this;
+    }
+}
+
+export class SlideNav extends Slide {
+    addArrow(prev, next) {
+        this.prevElement = document.querySelector(prev);
+        this.nextElement = document.querySelector(next);
+        this.addArrowEvent();
+    }
+
+    addArrowEvent() {
+        this.prevElement.addEventListener('click', this.activePrevSlide);
+        this.nextElement.addEventListener('click',this.activeNextSlide);
     }
 }
